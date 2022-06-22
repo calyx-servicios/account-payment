9ec0201fb91eade690e26755641de7da264d0d2e
cristian <crisdparadiso@gmail.com>
2022-06-22 13:46:41 -0300

    [ODOO-4045][ADD][account_withholding_min_taxeable]

---
+++ account_withholding_min_taxable/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Withholding Non-Taxable Minimum",
+    "summary": """
+        This module adds the posibility to add a Non-Taxable Minimum to withholdings.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["ParadisoCristian"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Account",
+    "version": "11.0.1.0.0",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_withholding_automatic",
+    ],
+}
